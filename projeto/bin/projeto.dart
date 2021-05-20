c854c1cd0d1e82fcaa1622ea4d6efcd370aed41f
santosraquel <raquel.dossantos1965@gmail.com>
2021-05-19 23:47:46 -0300

    revisão - variáveis e estrutura de decisão CORRETO

--- projeto/bin/projeto.dart
+++ projeto/bin/projeto.dart
@@ -3,21 +3,12 @@ import 'dart:io';
 void main(List<String> arguments) {
   print('Informe um número: ');
 
-  var entradaNumero1 = stdin.readLineSync();
-  var numero1 = int.parse(entradaNumero1);
+  var entradaNumero = stdin.readLineSync();
+  var numero = int.parse(entradaNumero);
 
-  if(numero1 %2 == 0){
+  if(numero %2 == 0){
     print('$numero é PAR');
   }else{
     print('$numero é ÍMPAR');
   }
-
-  print('Informe um número: ');
-  var entradaNumero2 = stdin.readLineSync();
-  var numero2 = int.parse(entradaNumero2);
-  
-  for(int i; i < numero2; i++){
-    
-  }
-
 }
